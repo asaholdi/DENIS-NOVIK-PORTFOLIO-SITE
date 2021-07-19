60e158b7d3e5c59fab900c3f5829bfb44d1b8a06
Саша Штоколов <81267831+asaholdi@users.noreply.github.com>
2021-07-19 18:39:14 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -14,42 +14,54 @@ window.onscroll = function showHeader() {
 /* HEADER CODE */
 
 var header = $('.header_content_js'),
-cloneHeader = header.clone()
+    cloneHeader = header.clone()
 
 cloneHeader.addClass('header--fixed')
 header.before(cloneHeader);
 
-$(window).scroll(function(){
+$(window).scroll(function () {
     console.log($(window).scrollTop());
 
-    if ( $(window).scrollTop() > 100 ){
+    if ($(window).scrollTop() > 100) {
         cloneHeader.addClass('header-show');
-    }
-    else{
+    } else {
         cloneHeader.removeClass('header-show');
     }
-}); 
+});
 
 /* BURGER CODE */
 
 const menuBurger = document.querySelector('.header_burger');
 if (menuBurger) {
     const menuBody = document.querySelector('.navbar');
-    menuBurger.addEventListener("click", function(e){
+    menuBurger.addEventListener("click", function (e) {
         document.body.classList.toggle('lock');
         menuBurger.classList.toggle('active');
         menuBody.classList.toggle('active');
     });
+
+    const anchors = document.getElementsByClassName('nav_link');
+    for (let i = 0; i < anchors.length; i++) {
+        anchors[i].onclick = function navbar() {
+            menuBurger.classList.toggle('active');
+            menuBody.classList.toggle('active');
+        }
+    }
+
 }
 
 
 /* BTN SCROLLTOP CODE */
 const scrollBtn = document.querySelector('.showBtn');
 window.onscroll = () => {
-    if(window.scrollY > 300) {
+    if (window.scrollY > 300) {
         document.querySelector('.showBtn').classList.remove('showBtn_hide');
-    }
-    else if(window.scrollY < 300) {
+    } else if (window.scrollY < 300) {
         scrollBtn.classList.add('showBtn_hide');
     };
 }
+
+// NAVBAR CODE
+
+/*
+ */
